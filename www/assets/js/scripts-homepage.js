f8891a431abbd8b6db4004662421cb5be17600ad
Jared Siebert <jared@siebertech.com>
2012-10-16 16:28:23 -0700

    Update page nivo sliders to start on ajax complete

--- www/assets/js/scripts-homepage.js
+++ www/assets/js/scripts-homepage.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	//Homepage Photogallery Connector
 	$('#slider').html('');
-	var galleryId = "faeca5fe-2264-112b-b112-de10559a1183";	//	TEMPLATE ONE PHOTOGALLERY ID	//
+	var galleryId = "faeca5fe-2264-112b-b112-de10559a1181";	//	TEMPLATE ONE PHOTOGALLERY ID	//
 	$.ajax({
     		type: "GET",
 		url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+galleryId+"&random=1303250264962.93",
--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -10,40 +10,35 @@ $(document).ready(function() {
 		$("#pagePhotoGallery img").css("display","none");
 		
 		$.ajax({
-	    	type: "GET",
-	        url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+galleryId+"&random=1303250264962.93",
-	        dataType: "xml",
-	        success: function(xml) {
+	    		type: "GET",
+			url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+galleryId+"&random=1303250264962.93",
+			dataType: "xml",
+			success: function(xml) {
 				$(xml).find('img').each(function() {
-				   var location = '/assets/images/photogallery/images/large/';
-				   var photo = $(this).attr('src');
-				   var alt = $(this).attr('caption');
-				   var url = $(this).attr('link');
+					var location = '/assets/images/photogallery/images/large/';
+					var photo = $(this).attr('src');
+					var alt = $(this).attr('caption');
+					var url = $(this).attr('link');
 					//$('<li></li>').html('<a href="'+location+''+url+'"><img src="'+location+''+url+'" alt="'+alt+'"/></a>').appendTo('#photogallery');
 					if (url == undefined) {
 						$('#pagePhotoGallery').append('<img src="'+location+''+photo+'" alt="'+alt+'"/>');
-					} else {
+					} else{
 						$('#pagePhotoGallery').append('<a href="'+url+'"><img src="'+location+''+photo+'" alt="'+alt+'"/></a>');	
 					}
 				});
-
-			}
-
+			},
+			complete: function(){
+	   			$('#pagePhotoGallery').nivoSlider({
+					effect:'fade', // Specify sets like: 'fold,fade,sliceDown'
+					slices:15, // For slice animations
+					animSpeed:500, // Slide transition speed
+					pauseTime:5000, // How long each slide will show
+					startSlide:0, // Set starting Slide (0 index)
+					directionNav:true, // Next & Prev navigation
+					directionNavHide:true, // Only show on hover
+					controlNav:true // 1,2,3... navigation	
+				});	
+	   		}
 	   	});
-		
-		setTimeout ( "showSlider()", 1000 );
 	}
 });
-
-function showSlider() {
-	$('#pagePhotoGallery').nivoSlider({
-		effect:'fade', // Specify sets like: 'fold,fade,sliceDown'
-		slices:15, // For slice animations
-		animSpeed:500, // Slide transition speed
-		pauseTime:5000, // How long each slide will show
-		startSlide:0, // Set starting Slide (0 index)
-		directionNav:true, // Next & Prev navigation
-		directionNavHide:true, // Only show on hover
-		controlNav:true // 1,2,3... navigation
-	});	
-}
